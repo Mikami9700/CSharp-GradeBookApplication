cfa1697a3e45a67e991652f2f4802383f63b46de
Mikami <mlouk9700@gmail.com>
2020-11-15 18:20:13 +0300

    Back to 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,4 @@
 ﻿using GradeBook.Enums;
-using System;
-using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -11,24 +9,5 @@ namespace GradeBook.GradeBooks
             Type = GradeBookType.Ranked;
         }
 
-        public override GetLetterGrade(double averageGrade)
-        {
-            if (Students.Count < 5)
-                throw new InvalidOperationException("You must have at least 5 students to do ranked grading.");
-
-            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
-            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
-
-            if (averageGrade >= grades[threshold - 1])
-                return 'A';
-            if (averageGrade >= grades[(threshold * 2) - 1])
-                return 'B';
-            if (averageGrade >= grades[(threshold * 3) - 1])
-                return 'C';
-            if (averageGrade >= grades[(threshold * 4) - 1])
-                return 'D';
-
-            return 'F';
-        }
     }
 }
